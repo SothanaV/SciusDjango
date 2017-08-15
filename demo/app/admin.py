b0b52ae803a53d8ec401be8ed3573c4c6174c8d8
wasit7 <wasit7@gmail.com>
2017-08-15 18:07:21 +0700

    cleaned admin page

--- demo/app/admin.py
+++ demo/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Note
+
+class NoteAdmin(admin.ModelAdmin):
+	list_display=[f.name for f in Note._meta.fields]
+
+admin.site.register(Note,NoteAdmin)
--- demo/app/models.py
+++ demo/app/models.py
@@ -2,10 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Person(models.Model):
-	created=models.DateTimeField(auto_now_add=False)
+class Note(models.Model):
+	created=models.DateTimeField(auto_now_add=True)
 	name=models.CharField(max_length=100)
-	email=EmailField()
+	email=models.EmailField()
 	message=models.TextField()
 	
 	def __unicode__(self):
