6555c06befb9b0422922b6e57b23b9516310c6e0
wasit7 <wasit7@gmail.com>
2017-08-15 18:20:02 +0700

    added http://localhost:8000/note/

--- demo/app/views.py
+++ demo/app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from app.models import Note
+from app.forms import NoteForm
+
+from django.shortcuts import render
+
+def home(request):
+	return render(request, 'home.html', {'key': "value" })
+
+class CreateNoteView(CreateView):
+	queryset = Note()
+	template_name='note.html'
+	form_class = NoteForm
+	success_url = '/'
